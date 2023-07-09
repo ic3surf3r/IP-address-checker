45b46a9cebca5a0f3a9d8cf6e81e1d6cf3602c53
ic3surf3r <novis.joao@gmail.com>
2023-07-09 21:36:14 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex w-screen justify-center">
+      <h1 className="text-3xl font-bold p-32">
+        Tailwind installed, we are ready to start.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
