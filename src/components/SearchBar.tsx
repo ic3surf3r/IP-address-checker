ba5bf80048fabd2eb7c19f46d4bbf1b87e8b61fb
ic3surf3r <novis.joao@gmail.com>
2023-07-10 16:18:27 +0200

    added react-responsive and different placeholder for large and small screens

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,18 @@
 import arrowIcon from "../assets/icon-arrow.svg";
+import { useMediaQuery } from "react-responsive";
 
 function SearchBar() {
+  const isSmallScreen = useMediaQuery({ maxWidth: 640 });
   return (
     <div className="w-4/5 flex shadow-md">
       <input
         type="text"
         className="bg-white w-5/6 rounded-xl rounded-r-none p-4 font-rubik"
+        placeholder={
+          isSmallScreen
+            ? "Search for any IP or domain"
+            : "Seach for any IP address or domain"
+        }
       />
       <div className="bg-black rounded-xl rounded-l-none flex justify-center items-center w-1/6">
         <img src={arrowIcon} alt="Go" />
