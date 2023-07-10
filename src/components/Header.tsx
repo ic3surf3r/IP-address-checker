225e8d7713a1cc7dd1c558cd2afa8f17f3169d00
ic3surf3r <novis.joao@gmail.com>
2023-07-10 16:11:53 +0200

    Search-Bar and Heading done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import SearchBar from "./SearchBar";
+
 function Header() {
   return (
-    <div className="bg-cover bg-no-repeat bg-center bg-mobile md:bg-desktop w-screen h-1/3 flex flex-col justify-center items-center">
-      <h1 className="text-white font-rubik text-2xl">IP Address Tracker</h1>
+    <div className="bg-cover bg-no-repeat bg-center bg-mobile md:bg-desktop w-screen h-1/3 flex flex-col  items-center relative">
+      <h1 className="text-white font-rubik text-2xl mt-7">
+        IP Address Tracker
+      </h1>
+      <br />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,17 @@
+import arrowIcon from "../assets/icon-arrow.svg";
+
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div className="w-4/5 flex shadow-md">
+      <input
+        type="text"
+        className="bg-white w-5/6 rounded-xl rounded-r-none p-4 font-rubik"
+      />
+      <div className="bg-black rounded-xl rounded-l-none flex justify-center items-center w-1/6">
+        <img src={arrowIcon} alt="Go" />
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
